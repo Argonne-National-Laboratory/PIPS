ad03d1a9ef1c1ff09a28397ebc4f052110416a0c
mlubin <mlubin@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-03-27 20:13:42 +0000

    fixes to compile on BGP

---
+++ BGPToolchain.cmake
@@ -0,0 +1,29 @@
+set(CMAKE_SYSTEM_NAME BlueGeneP-static)
+
+# in build directory:
+# cmake -DCMAKE_TOOLCHAIN_FILE=../BGPToolchain.cmake ..
+
+set(CMAKE_C_COMPILER mpixlc_r)
+set(CMAKE_CXX_COMPILER mpixlcxx_r)
+set(CMAKE_Fortran_COMPILER bgxlf_r)
+
+set(CMAKE_CXX_FLAGS_DEBUG "-g -O0")
+#set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O4")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g -O2 -qfloat=maf")
+
+#set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} /home/mlubin/flex-2.5.35)
+
+set(BGP_LAPACK "/soft/apps/LAPACK/liblapack_bgp.a")
+set(PURE_ESSL "/soft/apps/ESSL-4.4.1-1/lib/libesslbg.a")
+
+set(IBMCMP_BASE "/soft/apps/ibmcmp-dec2011")
+set(XLF_BASE "${IBMCMP_BASE}/xlf/bg/11.1/bglib")
+set(XLF_LIBS "-L${XLF_BASE} -lxlfmath -lxlf90_r")
+
+set(XLSMP_BASE "${IBMCMP_BASE}/xlsmp/bg/1.7/bglib")
+set(XLOMP_SER "-L${XLSMP_BASE} -lxlomp_ser")
+set(XLSMP "-L${XLSMP_BASE} -lxlsmp")
+
+set(MATH_LIBS "${BGP_LAPACK};${PURE_ESSL};${XLF_LIBS};${XLSMP}")
+
+set(BOOST_ROOT "/home/mlubin/boost_1_47_0")
