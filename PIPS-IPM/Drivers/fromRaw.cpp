ca03ba38297189693f7d4f37bdb2c807f3ab281c
petra <petra@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-05-14 22:06:35 +0000

    Intermediary changes

--- PIPS-IPM/Drivers/fromRaw.cpp
+++ PIPS-IPM/Drivers/fromRaw.cpp
@@ -3,6 +3,26 @@
 
 using namespace std;
 
-int main(int arg, char ** argv) {
+int main(int argc, char ** argv) {
+
+  MPI_Init(&argc, &argv);
+  int mype; MPI_Comm_rank(MPI_COMM_WORLD,&mype);
+
+  if(argc<3) {
+    if (mype == 0) printf("Usage: %s [rawdump root name] [num scenarios] [solution output root name]\n",argv[0]);
+    return 1;
+  }
+  
+  string datarootname(argv[1]);
+  int nscen = atoi(argv[2]);
+
+  rawInput* s = new rawInput(datarootname,nscen);
+  
+  PIPSIPMInterface pipsIpm(*s);
+  pipsIpm.loadData();
+  pipsIpm.go();
+
+  delete s;
+
   return 0;
 }
