800954e8991d17e9b001c55c615f9432c3764b64
mlubin <mlubin@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-05-25 22:45:48 +0000

    implement interface for ooqp taking stochasticInput

---
+++ PIPS-IPM/Drivers/ooqpFromRaw.cpp
@@ -0,0 +1,31 @@
+#include "rawInput.hpp"
+#include "OOQPInterface.hpp"
+#include "QpGenSparseMa57.h"
+#include "MehrotraSolver.h"
+
+using namespace std;
+
+int main(int argc, char ** argv) {
+
+  MPI_Init(&argc, &argv);
+  int mype; MPI_Comm_rank(MPI_COMM_WORLD,&mype);
+
+  if(argc<3) {
+    if (mype == 0) printf("Usage: %s [rawdump root name] [num scenarios] [solution output root name]\n",argv[0]);
+    return 1;
+  }
+  
+  string datarootname(argv[1]);
+  int nscen = atoi(argv[2]);
+
+  rawInput* s = new rawInput(datarootname,nscen);
+  
+  OOQPInterface<MehrotraSolver,QpGenSparseMa57> ooqp(*s);
+
+  delete s;
+
+  ooqp.go();
+  
+
+  return 0;
+}
