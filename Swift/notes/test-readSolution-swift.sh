66cb45a494ead281bfb86785c06d60c4f645ac5f
wozniak <wozniak@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-06-13 21:58:57 +0000

    Actually store blob

--- Swift/notes/test-readSolution-swift.sh
+++ Swift/notes/test-readSolution-swift.sh
@@ -2,7 +2,8 @@
 
 # Compile and run Swift script that links to functions
 
-export TURBINE_USER_LIB=/home/wozniak/collab/PIPS/Swift/notes
+SWIG_DATA=${HOME}/exm/apps/swig-data
+export TURBINE_USER_LIB="/home/wozniak/collab/PIPS/Swift/notes ${SWIG_DATA}"
 
 stc -u test-readSolution.{swift,tcl}
 
