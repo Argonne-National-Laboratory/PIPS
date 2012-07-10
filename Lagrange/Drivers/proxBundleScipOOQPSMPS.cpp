af5fc0333130a89f26be674e57544fd4987845e2
mlubin <mlubin@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-07-10 22:02:15 +0000

    support for adding multiple cuts at each iteration

--- Lagrange/Drivers/proxBundleScipOOQPSMPS.cpp
+++ Lagrange/Drivers/proxBundleScipOOQPSMPS.cpp
@@ -33,6 +33,7 @@ int main(int argc, char **argv) {
 	string smpsrootname(argv[1]);
 
 	SMPSInput input(smpsrootname+".cor",smpsrootname+".tim",smpsrootname+".sto");
+	if (mype == 0) printf("%d scenarios %d first-stage vars %d first-stage constr %d second-stage vars %d second-stage constr\n",input.nScenarios(),input.nFirstStageVars(),input.nFirstStageCons(),input.nSecondStageVars(0),input.nSecondStageCons(0)); 
 	
 	BAContext ctx(MPI_COMM_WORLD);
 	ctx.initializeAssignment(input.nScenarios());
