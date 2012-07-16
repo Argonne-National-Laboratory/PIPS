0cf06438d7092721f5397625e7bcb0c2b15b24e8
wozniak <wozniak@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-07-16 19:31:05 +0000

    Initial Swift test for evaluateRecourseLP()

---
+++ Swift/notes/test-evaluateRecourseLP-swift.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Compile and run Swift script that links to functions
+
+SWIG_DATA=${HOME}/exm/apps/swig-data
+export TURBINE_USER_LIB="/home/wozniak/collab/PIPS/Swift/notes ${SWIG_DATA}"
+
+stc -u test-evaluateRecourseLP.{swift,tcl}
+
+turbine -l -n 3 test-evaluateRecourseLP.tcl
