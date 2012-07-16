4d01745838b9b1e13d66d4533a389a0bc8b40d9f
wozniak <wozniak@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-07-16 19:30:23 +0000

    Initial Swift declaration for evaluateRecourseLP()

---
+++ Swift/notes/rounding_functions.swift
@@ -0,0 +1,9 @@
+
+#ifndef ROUNDING_FUNCTIONS_SWIFT
+#define ROUNDING_FUNCTIONS_SWIFT
+
+(blob b) evaluateRecourseLP(string dataPath, int nScen,
+                            int scen, blob candidateSolution)
+"rounding_functions" "0.0" "evaluateRecourseLP_turbine";
+
+#endif
