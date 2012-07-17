3f7a223aa3fd35e32587966410e97ddff60bdd4d
mlubin <mlubin@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-07-17 17:46:00 +0000

    add comments

--- BGPToolchain.cmake
+++ BGPToolchain.cmake
@@ -2,7 +2,8 @@ set(CMAKE_SYSTEM_NAME BlueGeneP-static)
 
 # in build directory:
 # cmake -DCMAKE_TOOLCHAIN_FILE=../BGPToolchain.cmake ..
-
+# for Cbc:
+# CXX="bgxlc++_r" ./configure --enable-static=yes --enable-shared=no --host=powerpc-bgp-linux
 set(CMAKE_C_COMPILER mpixlc_r)
 set(CMAKE_CXX_COMPILER mpixlcxx_r)
 set(CMAKE_Fortran_COMPILER bgxlf_r)
@@ -16,7 +17,9 @@ set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g -O2 -qfloat=maf")
 set(BGP_LAPACK "/soft/apps/LAPACK/liblapack_bgp.a")
 set(PURE_ESSL "/soft/apps/ESSL-4.4.1-1/lib/libesslbg.a")
 
-set(IBMCMP_BASE "/soft/apps/ibmcmp-dec2011")
+# set this version to match the compiler used
+# below corresponds to @ibm-compilers-apr2012 in softenv
+set(IBMCMP_BASE "/soft/apps/ibmcmp-apr2012")
 set(XLF_BASE "${IBMCMP_BASE}/xlf/bg/11.1/bglib")
 set(XLF_LIBS "-L${XLF_BASE} -lxlfmath -lxlf90_r")
 
@@ -27,3 +30,4 @@ set(XLSMP "-L${XLSMP_BASE} -lxlsmp")
 set(MATH_LIBS "${BGP_LAPACK};${PURE_ESSL};${XLF_LIBS};${XLSMP}")
 
 set(BOOST_ROOT "/home/mlubin/boost_1_47_0")
+set(OpenMP_CXX_FLAGS "-qsmp=omp")
