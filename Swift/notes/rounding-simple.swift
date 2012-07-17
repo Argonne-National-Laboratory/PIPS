9bb6743e502df61a42d128f2e25cde81d13d0f76
wozniak <wozniak@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-07-17 17:17:37 +0000

    Almost compiles...

--- Swift/notes/rounding-simple.swift
+++ Swift/notes/rounding-simple.swift
@@ -1,20 +1,28 @@
 
-# problem data
-string dataPath = "...";
-string solutionPath = "...";
-int nScenarios = ...;
-blob s = readConvSolution(dataPath,solutionPath);
+#include <builtins.swift>
+#include <swift/stdio.swift>
 
-# eventually we want to do a sweep over different values of cutoff
-# and possibly other functions in the place of "round"
-float cutoff = 0.5;
-blob r = round(s,cutoff);
+#include "rounding_functions.swift"
 
-# inner loop, given "r"
-float v[];
-foreach i in [0 : nScenarios-1]
-{
-  v[i] = evaluateRecourseLP(dataPath,nScenarios,i,r);
+main {
+  // problem data
+  string dataPath = "file.data";
+  string solutionPath = "file.solution";
+  int nScenarios = 3;
+  blob s = readConvSolution(dataPath,solutionPath);
+
+  // eventually we want to do a sweep over different values of cutoff
+  // and possibly other functions in the place of "round"
+  float cutoff = 0.5;
+  blob r = round(s,cutoff);
+
+  // inner loop, given "r"
+  float v[];
+  foreach i in [0 : nScenarios-1]
+  {
+    // TODO: Should this return a float or a blob?
+    v[i] = evaluateRecourseLP(dataPath,nScenarios,i,r);
+  }
+  float result = sum_float(v);
+  printf("result: %f\n", result);
 }
-float result = sum(v);
-printf("result: %f\n", result);
