0bb3fa2eb4ae094a7e4183d1431cbfd23fc90f46
wozniak <wozniak@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-07-19 18:39:17 +0000

    Catch compile errors

--- Swift/notes/run-rounding-simple.sh
+++ Swift/notes/run-rounding-simple.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 export TURBINE_HOME=/home/wozniak/Public/turbine-0.0.4-x86_64
-stc rounding-simple.{swift,tcl}
 
 SWIG_DATA=${HOME}/exm/apps/swig-data
 export TURBINE_USER_LIB="${PWD} ${SWIG_DATA}"
+
+stc -u rounding-simple.{swift,tcl}
+[[ ${?} == 0 ]] || exit 1
 
 turbine -l -n 3 rounding-simple.tcl
