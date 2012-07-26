552f917b1f1a88ca6025ed4f78cbcabdee3c6fa4
petra <petra@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-07-26 23:23:20 +0000

    The driver for the Schur complement approach.

---
+++ PIPS-IPM/Drivers/pipsipmFromRaw_schur.cpp
@@ -0,0 +1,66 @@
+/* PIPS-IPM                                                           *
+ * Author:  Cosmin G. Petra                                           *
+ * (C) 2012 Argonne National Laboratory. See Copyright Notification.  */
+
+#include "rawInput.hpp"
+#include "sInterfaceCallbacks.h"
+#include "PIPSIpmInterface.h"
+
+#include "sFactoryAugSchurLeaf.h"
+#include "MehrotraStochSolver.h"
+
+using namespace std;
+
+// using the latest interface PIPSIpmInterface 
+int solve               (const string& datarootname, int nscen);
+
+
+int main(int argc, char ** argv) {
+  MPI_Init(&argc, &argv);
+  int mype; MPI_Comm_rank(MPI_COMM_WORLD,&mype);
+
+  if(argc<3) {
+    if (mype == 0) printf("Usage: %s [rawdump root name] [num scenarios] [solution output root name]\n",argv[0]);
+    return 1;
+  }
+  
+  string datarootname(argv[1]);
+  int nscen = atoi(argv[2]);
+
+  //solve_usingCallbacks(datarootname, nscen);
+  solve(datarootname, nscen);
+
+
+  MPI_Finalize();
+  return 0;
+}
+
+
+int solve(const string& datarootname, int nscen) {
+  
+  rawInput* s = new rawInput(datarootname,nscen);
+  PIPSIpmInterface<sFactoryAugSchurLeaf, MehrotraStochSolver> pipsIpm(*s);
+  delete s;
+
+  pipsIpm.go();
+
+
+
+
+
+  return 0;
+}
+
+
+int solve_usingCallbacks(const string& datarootname, int nscen) {
+
+  rawInput* s = new rawInput(datarootname,nscen);
+  sInterfaceCallbacks pipsIpm(*s);
+
+  pipsIpm.go();
+
+  // when using callbacks the input can be deleted only after optimization is finished.
+  delete s;
+
+  return 0;
+}
