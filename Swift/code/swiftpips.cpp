d556fc6c74dd2dc816e56a2bc4646d298cd863bc
petra <petra@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-07-28 01:48:49 +0000

    Added functions that cache "zero data"

--- Swift/code/swiftpips.cpp
+++ Swift/code/swiftpips.cpp
@@ -115,3 +115,106 @@ roundSolution(double *convSolution, int CS_length, double cutoff)
   printf("roundSolution() done.\n");
   return data;
 }
+
+/////////////////////////////////////////////////////////////////////
+// Functions that work with cached "zero" (1st stage) problem data.
+////////////////////////////////////////////////////////////////////
+
+// reads "zero" problem data and returns it in a blob
+struct Data*
+readStg1Data(const char *dataPath)
+{
+  printf("readStg1Data()...\n");
+  printf("dataPath: %s\n", dataPath);
+  string problemdata = string(dataPath) + "0";
+  printf("problemdata file: %s\n", problemdata.c_str());
+  ifstream datafd(problemdata.c_str());
+  assert(datafd.good());
+
+  string data((istreambuf_iterator<char>(datafd)),istreambuf_iterator<char>()); // read into string
+  datafd.close();
+
+  size_t datalen = data.size()+1; //include trailing \0
+
+  struct Data* data = (struct Data*) malloc(sizeof(struct Data));
+  data->pointer = malloc(datalen*sizeof(char));
+  data->length = datalen*sizeof(char);
+
+  memcpy(data->pointer, data.c_str(), datalen);
+
+  printf("read1StgData() done.\n");
+  return data;
+}
+
+// cache-friendly version of readConvSolution
+struct Data*
+readConvSolution2(char* zerodata, int ZD_length,
+		  const char *solutionPath)
+		  
+{
+  printf("readConvSolution2()...\n");
+
+  istringstream f1( string(zerodata) );
+  f1.exceptions(ifstream::failbit | ifstream::badbit);
+  int nvar1;
+  f1 >> nvar1; // read size of solution
+  printf("nvar1: %i\n", nvar1);
+
+  struct Data* data = (struct Data*) malloc(sizeof(struct Data));
+  data->pointer = malloc(nvar1*sizeof(double));
+  data->length = nvar1*sizeof(double);
+
+  double *vec = reinterpret_cast<double*>(data->pointer);
+
+  ifstream solfd(solutionPath);
+  for (int i = 0; i < nvar1; i++) {
+    solfd >> vec[i];
+  }
+
+  printf("readConvSolution2() done.\n");
+  return data;
+}
+
+// evaluateRecourseLP2 - uses cached zero data
+double
+evaluateRecourseLP2(const char *dataPath, int nScen,
+		    double *candidateSolution, int CS_length,
+		    char* zerodata, int ZD_length)
+{
+  printf("evaluateRecourseLP2()...\n");
+  int N = CS_length/sizeof(double);
+  printf("N: %i\n", N);
+  vector<double> sol(candidateSolution,candidateSolution+N);
+
+  printf("ok0\n");
+
+  rawInput input(string(dataPath), string(zerodata), nScen);
+
+  printf("ok.5\n");
+
+  int nvar1 = input.nFirstStageVars();
+  printf("nvar1: %i\n", nvar1);
+  assert(nvar1 == N);
+
+  printf("ok1\n");
+
+  ClpRecourseSolver rsol(input, scen, sol);
+  rsol.setDualObjectiveLimit(1e7);
+
+  rsol.go();
+
+  double obj = rsol.getObjective();
+  if (rsol.getStatus() == ProvenInfeasible) {
+    return COIN_DBL_MAX;
+  }
+  assert(rsol.getStatus() == Optimal);
+
+  double prob = input.scenarioProbability(scen);
+  const vector<double> &obj1 = input.getFirstStageObj();
+
+  for (int k = 0; k < nvar1; k++) obj += prob*sol[k]*obj1[k];
+
+  printf("evaluateRecourseLP2() done.\n");
+  return obj;
+
+}
