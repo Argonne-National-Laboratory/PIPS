ab4bf17aad21b19164813daa68898609edb6a36f
petra <petra@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-07-30 21:04:52 +0000

    Intermediary update for PARDISO Aug Schur approach.

--- PIPS-IPM/Core/QpStoch/sLinsysLeafSchurSlv.C
+++ PIPS-IPM/Core/QpStoch/sLinsysLeafSchurSlv.C
@@ -11,11 +11,29 @@
 #include "PardisoSolver.h"
 #include "Ma57Solver.h"
 
-
+/**
+ * Computes U = Gi * inv(H_i) * Gi^T.
+ *        [ R 0 0 ]
+ * Gi^T = [ A 0 0 ]
+ *        [ C 0 0]
+ *
+ * A and C are the recourse eq. and ineq. matrices, R is the cross
+ * Hessian term.
+ */
 void sLinsysLeafSchurSlv::addTermToDenseSchurCompl( sData *prob, 
 						    DenseSymMatrix& SC)
 {
 
   cout  << " sLinsysLeafSchurSlv::addTermToDenseSchurComp is apparently called!" << endl;
-  sLinsysLeaf::addTermToDenseSchurCompl( prob, SC);
+  SparseGenMatrix& A = prob->getLocalA();
+  SparseGenMatrix& C = prob->getLocalC();
+  SparseGenMatrix& R = prob->getLocalCrossHessian();
+
+  int tmp, nxP, NP;
+  A.getSize(tmp, nxP); assert(tmp==locmy);
+  NP = SC.size();      assert(NP>=nxP);
+
+
+  
+
 }
