f889c83ee333d56fdbdcc26b0c8d4765ce0bebd3
petra <petra@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-08-15 21:10:30 +0000

    added code that outputs the primal and duals variables

--- PIPS-IPM/Drivers/pipsipmFromRaw_schur.cpp
+++ PIPS-IPM/Drivers/pipsipmFromRaw_schur.cpp
@@ -1,18 +1,19 @@
 /* PIPS-IPM                                                           *
  * Author:  Cosmin G. Petra                                           *
  * (C) 2012 Argonne National Laboratory. See Copyright Notification.  */
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "rawInput.hpp"
-#include "sInterfaceCallbacks.h"
 #include "PIPSIpmInterface.h"
 
 #include "sFactoryAugSchurLeaf.h"
 #include "MehrotraStochSolver.h"
 
-using namespace std;
+#include <string>
+#include <sstream>
 
-// using the latest interface PIPSIpmInterface 
-int solve               (const string& datarootname, int nscen);
+using namespace std;
 
 
 int main(int argc, char ** argv) {
@@ -20,47 +21,39 @@ int main(int argc, char ** argv) {
   int mype; MPI_Comm_rank(MPI_COMM_WORLD,&mype);
 
   if(argc<3) {
-    if (mype == 0) printf("Usage: %s [rawdump root name] [num scenarios] [solution output root name]\n",argv[0]);
+    if (mype == 0) printf("Usage: %s [rawdump root name] [num scenarios] [solution output root name] [eq mult root name]\n",argv[0]);
     return 1;
   }
   
   string datarootname(argv[1]);
   int nscen = atoi(argv[2]);
 
-  //solve_usingCallbacks(datarootname, nscen);
-  solve(datarootname, nscen);
-
-
-  MPI_Finalize();
-  return 0;
-}
-
-
-int solve(const string& datarootname, int nscen) {
-  
   rawInput* s = new rawInput(datarootname,nscen);
   PIPSIpmInterface<sFactoryAugSchurLeaf, MehrotraStochSolver> pipsIpm(*s);
   delete s;
 
   pipsIpm.go();
 
+  for(int s=0; s<nscen; s++) {
 
+    stringstream ss1; ss1<<"out_duals_scen"<<(s+1)<<".txt";
+    cout << "saving duals to " << ss1.str() << endl;
+    ofstream fileduals(ss1.str().c_str());
+    std::vector<double> duals = pipsIpm.getSecondStageDualRowSolution(s);
+    for(size_t i=0; i<duals.size(); i++)
+      fileduals << duals[i] << endl;
+    fileduals.close();
 
+    stringstream ss2; ss2<<"out_primals_scen"<<(s+1)<<".txt";
+    cout << "saving primals to " << ss2.str() << endl;
+    ofstream fileprimals(ss2.str().c_str());
+    std::vector<double> primals = pipsIpm.getSecondStagePrimalColSolution(s);
+    for(size_t i=0; i<primals.size(); i++)
+      fileprimals << primals[i] << endl;
+    fileprimals.close();
+  }
 
-
+  MPI_Finalize();
   return 0;
 }
 
-
-int solve_usingCallbacks(const string& datarootname, int nscen) {
-
-  rawInput* s = new rawInput(datarootname,nscen);
-  sInterfaceCallbacks pipsIpm(*s);
-
-  pipsIpm.go();
-
-  // when using callbacks the input can be deleted only after optimization is finished.
-  delete s;
-
-  return 0;
-}
