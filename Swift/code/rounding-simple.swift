5d2fc0d3db9635e586b2178a517670727f46393c
wozniak <wozniak@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-10-02 17:23:24 +0000

    Minor improvements

--- Swift/code/rounding-simple.swift
+++ Swift/code/rounding-simple.swift
@@ -8,15 +8,17 @@
 #include <builtins.swift>
 #include <swift/stdio.swift>
 #include <swift/stats.swift>
+#include <swift/unistd.swift>
 
 #include "pips.swift"
 
 main {
   // problem data
-  string data = "/home/wozniak/Public/PIPS.data/";
-  string dataPath = data + "uc_dumps/uc_raw-4h";
+  string data = "/home/wozniak/PIPS-data-2/";
+  // string dataPath = data + "uc_dumps/uc_raw-4h";
+  string dataPath = data + "4h_dump/uc_4h";
   string solutionPath = data + "primalsol_conv8";
-  int nScenarios = 4;
+  int nScenarios = toint(argv("N"));
   blob s = readConvSolution(dataPath,solutionPath);
 
   // eventually we want to do a sweep over different values of cutoff
@@ -29,7 +31,12 @@ main {
   foreach i in [0 : nScenarios-1]
   {
     v[i] = evaluateRecourseLP(dataPath,nScenarios,i,r);
-  }
+    printf("v[%i]=%f", i, v[i]);
+ }
   float result = sum_float(v);
   printf("result: %f\n", result);
+
+  // Output some metadata
+  int procs = adlb_servers() + turbine_engines() + turbine_workers();
+  printf("procs: %i", procs);
 }
