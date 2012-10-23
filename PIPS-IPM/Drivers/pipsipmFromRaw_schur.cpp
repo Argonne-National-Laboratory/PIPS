91275f66e464d61b2f1ff7cf72a334de2956f8e5
petra <petra@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-10-23 23:49:49 +0000

    saving the solution

--- PIPS-IPM/Drivers/pipsipmFromRaw_schur.cpp
+++ PIPS-IPM/Drivers/pipsipmFromRaw_schur.cpp
@@ -40,26 +40,42 @@ int main(int argc, char ** argv) {
   pipsIpm.go();
 
   if(mype==0) cout << "solving done" << endl;
-/*
+
+  if(mype==0) cout << "Saving solution" << endl;
   for(int s=0; s<nscen; s++) {
 
-    stringstream ss1; ss1<<"out_duals_scen"<<(s+1)<<".txt";
-    cout << "saving duals to " << ss1.str() << endl;
-    ofstream fileduals(ss1.str().c_str());
     std::vector<double> duals = pipsIpm.getSecondStageDualRowSolution(s);
-    for(size_t i=0; i<duals.size(); i++)
-      fileduals << duals[i] << endl;
-    fileduals.close();
+    if(duals.size()) {
+      stringstream ss1; ss1<<"out_duals_scen"<<(s+1)<<".txt";
+      cout << "saving duals to " << ss1.str() << endl;
+      ofstream fileduals(ss1.str().c_str());
+      
+      for(size_t i=0; i<duals.size(); i++)
+	fileduals << duals[i] << endl;
+      fileduals.close();
+    }
 
-    stringstream ss2; ss2<<"out_primals_scen"<<(s+1)<<".txt";
-    cout << "saving primals to " << ss2.str() << endl;
-    ofstream fileprimals(ss2.str().c_str());
     std::vector<double> primals = pipsIpm.getSecondStagePrimalColSolution(s);
-    for(size_t i=0; i<primals.size(); i++)
-      fileprimals << primals[i] << endl;
-    fileprimals.close();
+    if(primals.size()) {
+      stringstream ss2; ss2<<"out_primals_scen"<<(s+1)<<".txt";
+      cout << "saving primals to " << ss2.str() << endl;
+      ofstream fileprimals(ss2.str().c_str());
+      std::vector<double> primals = pipsIpm.getSecondStagePrimalColSolution(s);
+      for(size_t i=0; i<primals.size(); i++)
+	fileprimals << primals[i] << endl;
+      fileprimals.close();
+    }
   }
-*/
+  if(mype==0) {
+    std::vector<double> firstStageSol = pipsIpm.getFirstStagePrimalColSolution();
+    string sFile = "out_primal_1stStage.txt";
+    cout << "saving 1st stage sol to " << sFile << endl;
+    ofstream file1stStg(sFile.c_str());
+    for(size_t i=0; i<firstStageSol.size(); i++)
+      file1stStg << firstStageSol[i] << endl;
+    file1stStg.close();
+  }
+  cout << "Solution saved" << endl;
   MPI_Finalize();
   return 0;
 }
