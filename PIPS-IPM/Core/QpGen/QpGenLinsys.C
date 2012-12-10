f7193ca70605a6d2550e7312ba6837ae1073f97a
petra <petra@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2012-12-10 13:36:35 +0000

    Krylov on the entire linear system + updated output

--- PIPS-IPM/Core/QpGen/QpGenLinsys.C
+++ PIPS-IPM/Core/QpGen/QpGenLinsys.C
@@ -24,7 +24,7 @@
 #include <fstream>
 using namespace std;
 
-extern int gOuterIterRefin;
+extern int gOuterSolve;
 
 QpGenLinsys::QpGenLinsys( QpGen * factory_,
 			  QpGenData * prob,
@@ -51,21 +51,33 @@ QpGenLinsys::QpGenLinsys( QpGen * factory_,
   nomegaInv   = la->newVector( mz );
   rhs         = la->newVector( nx + my + mz );
 
-  if(gOuterIterRefin) {
-    //for iterative refinement
+  if(gOuterSolve) {
+    //for iterative refinement or BICGStab
     sol  = la->newVector( nx + my + mz );
     res  = la->newVector( nx + my + mz );
     resx = la->newVector( nx );
     resy = la->newVector( my );
     resz = la->newVector( mz );
+    if(gOuterSolve==2) {
+      //BiCGStab; additional vectors needed
+      sol2 = la->newVector( nx + my + mz );
+      res2 = la->newVector( nx + my + mz );
+      res3  = la->newVector( nx + my + mz );
+      res4  = la->newVector( nx + my + mz );
+      res5  = la->newVector( nx + my + mz );
+    } else {
+      sol2 = res2 = res3 = res4 = res5 = NULL;
+    }
   } else {
     sol = res = resx = resy = resz = NULL;
+    sol2 = res2 = res3 = res4 = res5 = NULL;
   }
 }
 
 QpGenLinsys::QpGenLinsys()
  : factory( NULL), rhs(NULL), dd(NULL), dq(NULL), useRefs(0),
-   sol(NULL), res(NULL), resx(NULL), resy(NULL), resz(NULL)
+   sol(NULL), res(NULL), resx(NULL), resy(NULL), resz(NULL),
+   sol2(NULL), res2(NULL), res3(NULL), res4(NULL), res5(NULL)
 {
 }
 
@@ -82,6 +94,11 @@ QpGenLinsys::~QpGenLinsys()
   if(resx) delete resx;
   if(resy) delete resy;
   if(resz) delete resz;
+  if(sol2) delete sol2;
+  if(res2) delete res2;
+  if(res3) delete res3;
+  if(res4) delete res4;
+  if(res5) delete res5;
   
 }
 void QpGenLinsys::factor(Data * /* prob_in */, Variables *vars_in)
@@ -244,20 +261,298 @@ void QpGenLinsys::solveXYZS( OoqpVector& stepx, OoqpVector& stepy,
 			       OoqpVector& /* ztemp */,
 			       QpGenData* prob )
 {
+  stepz.axzpy( -1.0, *nomegaInv, steps );
+ 
+  if(gOuterSolve==1) {
+    ///////////////////////////////////////////////////////////////
+    // Iterative refinement
+    ///////////////////////////////////////////////////////////////
+    solveCompressedIterRefin(stepx,stepy,stepz,prob);
 
+  } else if(gOuterSolve==0) {
+    ///////////////////////////////////////////////////////////////
+    // Default solve - Schur complement based decomposition
+    ///////////////////////////////////////////////////////////////
+    this->joinRHS( *rhs, stepx, stepy, stepz );
+    this->solveCompressed( *rhs );
+    this->separateVars( stepx, stepy, stepz, *rhs );
+
+  } else {
+    assert(gOuterSolve==2);
+    ///////////////////////////////////////////////////////////////
+    // BiCGStab
+    ///////////////////////////////////////////////////////////////
+    solveCompressedBiCGStab(stepx,stepy,stepz,prob);
+  }
+  stepy.negate();
+  stepz.negate();
+	
+  steps.axpy( -1.0, stepz );
+  steps.componentMult( *nomegaInv );
+  steps.negate();
+}
+
+
+void QpGenLinsys::solveCompressedBiCGStab(OoqpVector& stepx,
+					  OoqpVector& stepy,
+					  OoqpVector& stepz,
+					  QpGenData* data)
+{
+#ifdef TIMING
+  vector<double> histRelResid;
+  double tTot=MPI_Wtime(), tSlv=0., tResid=0., tTmp;
+#endif
+
+  this->joinRHS( *rhs, stepx, stepy, stepz );
+
+  //aliases
+  OoqpVector &r0=*res2, &dx=*sol2, &v=*res3, &t=*res4, &p=*res5;
+  OoqpVector &x=*sol, &r=*res, &b=*rhs;
+
+  const double tol=1e-10, EPS=2e-16; 
+  const int maxit=500;
+
+  double n2b=b.twonorm(), tolb=n2b*tol;
+  int flag; double iter=0.;
+  double rho=1., omega=1., alpha;  
+  int myRank; MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+
+
+#ifdef TIMING
+  tTmp=MPI_Wtime();
+#endif
+  //starting guess/point
+  x.copyFrom(b); solveCompressed(x); //solution to the approx. system
+#ifdef TIMING
+  tSlv += (MPI_Wtime()-tTmp);
+  tTmp=MPI_Wtime();
+#endif
+  //initial residual: res=res-A*x
+  r.copyFrom(b); matXYZMult(1.0,r, -1.0,x, data, stepx,stepy,stepz);
+#ifdef TIMING
+  tResid += (MPI_Wtime()-tTmp);
+#endif  
+  double normr=r.twonorm(), normr_min=normr, normr_act=normr;
+#ifdef TIMING
+  histRelResid.push_back(normr/n2b);
+#endif
+  
+  //quick return if solve is accurate enough
+  if(normr<tolb) { 
+     this->separateVars( stepx, stepy, stepz, x );
+#ifdef TIMING
+     tTot = MPI_Wtime() - tTot;
+     if(0==myRank) {
+       cout << "Outer BiCGStab 0 iterations. Rel.res.nrm:" << normr/n2b  << endl;
+       cout << "solveXYZS w/ BiCGStab times: solve=" << tSlv
+	    << "  matvec=" << tResid
+	    << "  total=" << tTot << endl; 
+     }
+#endif
+     return;
+  }
+
+  //arbitrary vector
+  r0.copyFrom(b);
+
+  //main loop
+  int it=0; while(it<maxit) {
+    flag=-1; //reset flag
+    double rho1=rho; double beta;
+    rho=r0.dotProductWith(r);
+    if(0.0==rho) {flag=4; break;}
+
+    //first half of the iterate
+    {
+      if(it==0) p.copyFrom(r);
+      else {
+	beta = (rho/rho1)*(alpha/omega);
+	if(beta==0.0) { flag=4; break; }
+	
+	//-------- p = r + beta*(p - omega*v) --------
+	p.axpy(-omega, v); p.scale(beta); p.axpy(1.0, r);
+      }
+#ifdef TIMING
+      tTmp = MPI_Wtime();
+#endif
+      //precond: ph = \tilde{K}^{-1} p
+      dx.copyFrom(p); solveCompressed(dx); 
+#ifdef TIMING
+      tSlv += (MPI_Wtime()-tTmp);
+      tTmp = MPI_Wtime();
+#endif
+      //mat-vec: v = K*ph
+      matXYZMult(0.0,v, 1.0,dx, data, stepx,stepy,stepz);
+#ifdef TIMING
+      tResid += (MPI_Wtime()-tTmp);
+#endif
+
+      double rtv=r0.dotProductWith(v);
+      if(rtv==0.0) { flag=4; break; }
+      
+      alpha=rho/rtv; 
+      // x = x + alpha*dx (x=x+alpha*ph)
+      x.axpy( alpha, dx);
+      // r = r-alpha*v (s=r-alpha*v)
+      r.axpy(-alpha, v);
+      
+      //check for convergence
+      normr=r.twonorm();
+#ifdef TIMING
+      histRelResid.push_back(normr/n2b);
+#endif
+      if(normr<=tolb) {
+#ifdef TIMING
+      tTmp=MPI_Wtime();
+#endif
+	//compute the actual residual
+	OoqpVector& res=dx; //use dx 
+	res.copyFrom(b); matXYZMult(1.0,res, -1.0,x, data, stepx,stepy,stepz);
+#ifdef TIMING
+	tResid += (MPI_Wtime()-tTmp);
+#endif
+
+	normr_act = res.twonorm();
+	if(normr_act<=tolb) {
+	  //converged
+#ifdef TIMING
+	  histRelResid[histRelResid.size()-1]=normr_act/n2b;
+#endif
+	  flag=0; iter=it+0.5; break;
+	}
+      } //~end of convergence test
+    }
+
+    
+    //second half of the iterate now
+    {
+#ifdef TIMING
+      tTmp=MPI_Wtime();
+#endif
+      //preconditioner
+      dx.copyFrom(r); solveCompressed(dx);
+#ifdef TIMING
+      tSlv += (MPI_Wtime()-tTmp);
+      tTmp=MPI_Wtime();
+#endif
+      //mat-vec
+      matXYZMult(0.0,t, 1.0,dx, data, stepx,stepy,stepz);
+#ifdef TIMING
+      tResid += (MPI_Wtime()-tTmp);
+#endif
+      double tt = t.dotProductWith(t);
+      if(tt==0.0) { flag=4; break;}
+      
+      omega=t.dotProductWith(r); omega /= tt;
+      
+      // x=x+omega*dx  (x=x+omega*sh)
+      x.axpy( omega, dx);
+      // r = r-omega*t (r=s-omega*sh)
+      r.axpy(-omega, t);
+      //check for convergence
+      normr=r.twonorm();
+
+      histRelResid.push_back(normr/n2b);
+      if(normr<=tolb) {
+#ifdef TIMING
+      tTmp=MPI_Wtime();
+#endif
+	//compute the actual residual
+	OoqpVector& res=dx; //use dx 
+	res.copyFrom(b); matXYZMult(1.0,res, -1.0,x, data, stepx,stepy,stepz);
+#ifdef TIMING
+      tResid += (MPI_Wtime()-tTmp);
+#endif
+
+	normr_act = res.twonorm();
+	//cout << "Outer BiCG - actual rel.res.nrm: " << normr_act/n2b << endl;
+	if(normr_act<=tolb) {
+	  //converged
+#ifdef TIMING 
+	  histRelResid[histRelResid.size()-1]=normr_act/n2b;
+#endif
+	  flag=0; iter=it+1.; break;
+	} // else continue - To Do: detect stagnation (flag==3)
+      } else {
+	//To Do: detect stagnation/divergence and rollback to min.norm. iterate
+	//for now we print a warning in case residual increases.
+	if(normr>normr_min) {
+	  if(0==myRank) 
+	    cout << "Outer BiCG - Increase in BiCGStab residual. Old=" << normr_min
+		 << "  New=" << normr << endl;
+	  flag=5;
+	  break; 
+	} else normr_min=normr;
+	
+      } //~end of convergence test
+    } //~end of scoping
+    
+    it++;
+  } //~ end of BiCGStab loop
+  
+  //warning/error messaging
+  if(flag!=0) {
+    if(0==myRank) 
+      cout << "Outer BiCG - convergence issues: flag=" << flag << ". "
+	   << iter << " iterations" 
+	   << " rel.res.norm=" << normr_act/n2b << endl;
+  }
+  this->separateVars( stepx, stepy, stepz, x );
+
+#ifdef TIMING
+  tTot = MPI_Wtime()-tTot;
+  if(0==myRank) {
+    cout << "Outer BiCGStab " << iter << " iterations. Rel.res.nrm:";
+    for(size_t it=0; it<histRelResid.size(); it++)
+      cout << histRelResid[it] << " | ";    
+    cout << endl;
+    cout << "solveXYZS w/ BiCGStab times: solve=" << tSlv
+	 << "  matvec=" << tResid
+	 << "  total=" << tTot << endl; 
+  }
+#endif  
+
+}
+
+
+/**
+ * res = beta*res - alpha*mat*sol
+ * stepx, stepy, stepz are used as temporary buffers
+ */
+void QpGenLinsys::matXYZMult(double beta,  OoqpVector& res, 
+			     double alpha, OoqpVector& sol, 
+			     QpGenData* data,
+			     OoqpVector& solx, 
+			     OoqpVector& soly, 
+			     OoqpVector& solz)
+{
+  this->separateVars( solx, soly, solz, sol );
+  this->separateVars( *resx, *resy, *resz, res);
+
+  data->Qmult(beta, *resx, alpha, solx);
+  resx->axzpy(alpha, *dd, solx);
+  data->ATransmult(1.0, *resx, alpha, soly);
+  data->CTransmult(1.0, *resx, alpha, solz);
+
+  data->Amult(beta, *resy, alpha, solx);
+  //cout << "resy norm: " << resy->twonorm() << endl;
+  data->Cmult(beta, *resz, alpha, solx);
+  resz->axzpy(alpha, *nomegaInv, solz);
+  //cout << "resz norm: " << resz->twonorm() << endl;
+  this->joinRHS( res, *resx, *resy, *resz );
+}
+
+void QpGenLinsys::solveCompressedIterRefin(OoqpVector& stepx,
+					   OoqpVector& stepy,
+					   OoqpVector& stepz,
+					   QpGenData* prob)
+{
+  this->joinRHS( *rhs, stepx, stepy, stepz );
 #ifdef TIMING
     int myRank; MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
     vector<double> histRelResid;
-    //vector<double> histRelResidInf;
-    if(0==myRank) cout << "solveXYZS solve ---" << endl;
-#endif
-  stepz.axzpy( -1.0, *nomegaInv, steps );
-  this->joinRHS( *rhs, stepx, stepy, stepz );
 
-  if(gOuterIterRefin) {
-    
-#ifdef TIMING
-      double tTot=MPI_Wtime(), tSlv=0., tResid=0., tTmp;
+    double tTot=MPI_Wtime(), tSlv=0., tResid=0., tTmp;
 #endif
     res->copyFrom(*rhs);
     sol->setToZero();
@@ -292,8 +587,8 @@ void QpGenLinsys::solveXYZS( OoqpVector& stepx, OoqpVector& stepy,
 #ifdef TIMING
       histRelResid.push_back(resNorm/bnorm);
       //histRelResidInf.push_back(res->infnorm()/bnorm);
-      if(0==myRank) cout << "resid.nrm xyz: " << resNorm << "   "
-			 << "rhs.nrm xyz: " << bnorm << endl;
+      //if(0==myRank) cout << "resid.nrm xyz: " << resNorm << "   "
+      //		 << "rhs.nrm xyz: " << bnorm << endl;
 #endif      
       
       if(resNorm/bnorm<1e-9)
@@ -303,26 +598,17 @@ void QpGenLinsys::solveXYZS( OoqpVector& stepx, OoqpVector& stepy,
 #ifdef TIMING
     tTot = MPI_Wtime() - tTot;
     if(0==myRank) {// && refinSteps>0)  {
-      cout << "Outer " << refinSteps << " iterative steps. Rel resids: "; //Norm rel res:" 
+      cout << "Outer Iter Refin " << refinSteps 
+	   << " iterations. Rel.resid.nrm:"; //Norm rel res:" 
       for(size_t it=0; it<histRelResid.size(); it++) 
 	  cout << histRelResid[it] << " | ";
       cout << endl;
-      cout << "Outer times:  solve=" << tSlv 
-	   << "  resid=" << tResid 
+      cout << "solveXYZS w/ iter. refin. times: solve=" << tSlv 
+	   << "  matvec=" << tResid 
 	   << "  total=" << tTot << endl; 
     }
 #endif
     this->separateVars( stepx, stepy, stepz, *sol );
-  } else {
-    this->solveCompressed( *rhs );
-    this->separateVars( stepx, stepy, stepz, *rhs );
-  }
-  stepy.negate();
-  stepz.negate();
-	
-  steps.axpy( -1.0, stepz );
-  steps.componentMult( *nomegaInv );
-  steps.negate();
 }
 
 /**
@@ -352,6 +638,7 @@ void QpGenLinsys::computeResidualXYZ(OoqpVector& sol,
   //cout << "resz norm: " << resz->twonorm() << endl;
   this->joinRHS( res, *resx, *resy, *resz );
 }
+
 
 void QpGenLinsys::joinRHS( OoqpVector& rhs_in,  OoqpVector& rhs1_in,
 			     OoqpVector& rhs2_in, OoqpVector& rhs3_in )
