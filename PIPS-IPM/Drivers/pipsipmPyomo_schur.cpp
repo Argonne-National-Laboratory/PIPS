bef577481f4ac35d642bea117f7ec43a3faa9a7a
petra <petra@fcef5e3e-b051-0410-8e2a-f9a80c2ea1c9>
2013-06-14 16:10:43 +0000

    pips pyomo changes

---
+++ PIPS-IPM/Drivers/pipsipmPyomo_schur.cpp
@@ -0,0 +1,60 @@
+/* PIPS-IPM - PYOMO wrapper and input                                 *
+ * Author:  Cosmin G. Petra                                           *
+ * (C) 2013 Argonne National Laboratory. See Copyright Notification.  */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "PyomoInput.hpp"
+#include "PIPSIpmInterface.h"
+
+#include "sFactoryAugSchurLeaf.h"
+#include "MehrotraStochSolver.h"
+
+#include <string>
+#include <sstream>
+
+using namespace std;
+extern int gOuterSolve;
+extern int gInnerSCsolve;
+
+int main(int argc, char ** argv) {
+  MPI_Init(&argc, &argv);
+  int mype; MPI_Comm_rank(MPI_COMM_WORLD,&mype);
+
+  if(argc<3) {
+    if (mype == 0) printf("Usage: %s [pyomo root name] [num scenarios] [outer solve (opt)] [inner solve (opt)]\n",argv[0]);
+    return 1;
+  }
+  
+  string datarootname(argv[1]);
+  int nscen = atoi(argv[2]);
+
+  int outerSolve=2;
+  if(argc>=4) {
+    outerSolve = atoi(argv[3]);
+    if(mype==0) cout << "Using option [" << outerSolve << "] for outer solve" << endl;
+  }
+
+  int innerSolve=0;
+  if(argc>=5) {
+    innerSolve = atoi(argv[4]);
+     if(mype==0) cout << "Using option [" << innerSolve << "] for inner solve" << endl;
+  }
+
+  if(mype==0) cout << argv[0] << " starting ..." << endl;
+  PyomoInput* s = new PyomoInput(datarootname,nscen);
+  if(mype==0) cout <<  "Pyomo input created from " << datarootname<< endl;
+  PIPSIpmInterface<sFactoryAugSchurLeaf, MehrotraStochSolver> pipsIpm(*s);
+  gOuterSolve=outerSolve;
+  gInnerSCsolve=innerSolve;
+
+  if(mype==0) cout <<  "PIPSIpmInterface created" << endl;
+  delete s;
+  if(mype==0) cout <<  "PyomoInput deleted ... starting to solve" << endl;
+
+  pipsIpm.go();
+
+  MPI_Finalize();
+  return 0;
+}
+
