edce5cc8043886f14ce4455a411b5271dae0c2c9
Geoffrey Oxberry <oxberry1@llnl.gov>
2015-02-17 16:30:10 -0800

    PIPS-S memleak: delete trailing whitespace

--- PIPS-S/Drivers/memleak.cpp
+++ PIPS-S/Drivers/memleak.cpp
@@ -11,13 +11,13 @@ using boost::scoped_ptr; // replace with unique_ptr for C++11
 using namespace std;
 
 int main(int argc, char **argv) {
-	
+
   cout << "Start" << endl;
   MPI_Init(&argc, &argv);
-  
+
   int mype;
   MPI_Comm_rank(MPI_COMM_WORLD,&mype);
-  
+
   if (argc < 3) {
     if (0 == mype) printf("Usage: %s [raw input root name] [num scenarios]\n", argv[0]);
     return 1;
@@ -27,7 +27,7 @@ int main(int argc, char **argv) {
   //  int nscen = 4;
   string datarootname(argv[1]);
   int nscen = atol(argv[2]);
-  
+
   scoped_ptr<rawInput> s(new rawInput(datarootname,nscen));
 
   BAContext ctx(MPI_COMM_WORLD);
@@ -41,35 +41,35 @@ int main(int argc, char **argv) {
   if (0) {
 
     vector<denseBAVector> v1,v2;
-    
+
     denseBAVector baVec1Copy;
-    
-    {   
+
+    {
       denseBAVector baVec1,baVec2;
       baVec1.allocate(dims, ctx, PrimalVector);
-      
+
       v1.push_back(baVec1);
-      
+
       baVec2.allocate(dims,ctx,PrimalVector);
       v1.push_back(baVec2);
       
       
       cout << "vector1 has " << v1.size() << " elements\n";
       denseBAVector baVec3 = v1[0];
-      
+
       //testing assignement operator
       baVec1Copy = v1[1];
-      
+
       baVec2 = baVec1;
-      
+
       v2.push_back(baVec3);
       v2.push_back(baVec1Copy);
       v2.push_back(baVec2);
     }
     denseBAVector baVec;
-    baVec.allocate(dims, ctx, PrimalVector);  
+    baVec.allocate(dims, ctx, PrimalVector);
     v2.push_back(baVec);
-    
+
     cout << "vector2 has " << v2.size() << " elements\n";
   }
 
@@ -112,7 +112,7 @@ int main(int argc, char **argv) {
     dualInfeas.getSecondStageVec(0).push_back(0);
     dualInfeas.getSecondStageVec(1).push_back(11);
 
-    
+
     BAContainer<vector<int> > dualInfeas2 =dualInfeas;
 
     BAContainer<vector<int> > dualInfeas3;
@@ -120,7 +120,7 @@ int main(int argc, char **argv) {
     cout << dualInfeas2.getVec(-1)[0] << " | " << dualInfeas2.getSecondStageVec(1)[0] << "\n";
     cout << dualInfeas3.getFirstStageVec()[0] << " | " << dualInfeas3.getSecondStageVec(1)[0] << "\n";
 
-    
+
   }
   MPI_Finalize();
   cout << "driver exiting ..." << endl;
