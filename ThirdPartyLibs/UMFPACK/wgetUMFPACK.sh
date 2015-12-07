3239db3beb0bba47f67dce56419744c4fe31a719
Feng Qiang <f.qiang@gmail.com>
2015-12-07 17:43:10 -0600

    mac os config for UMFPack

--- ThirdPartyLibs/UMFPACK/wgetUMFPACK.sh
+++ ThirdPartyLibs/UMFPACK/wgetUMFPACK.sh
@@ -4,13 +4,13 @@ echo "##### Downloading the third party packages for PIPS-NLP:"
 echo " "
 
 echo "### Downloading SuiteSparse:"
-if wget http://faculty.cse.tamu.edu/davis/SuiteSparse/${fn}
-then
-  echo "### SuiteSparse: Download Successful.\n"
-else
-  echo "### SuiteSparse: Download Failed.\n"
-  exit 1
-fi
+#if wget http://faculty.cse.tamu.edu/davis/SuiteSparse/${fn}
+#then
+#  echo "### SuiteSparse: Download Successful.\n"
+#else
+#  echo "### SuiteSparse: Download Failed.\n"
+#  exit 1
+#fi
 
 name=`tar -ztf ${fn} |  cut -f1 -d"/" | uniq`
 tar -xf ${fn}
@@ -20,4 +20,14 @@ cd src
 #need to install openblas-dev pakcage
 #use LD_LIBRARY_PATH is not suggested
 #LD_LIBRARY_PATH=/usr/local/lib/x86_64-linux-gnu
+cd SuiteSparse_config
+conf_file=SuiteSparse_config.mk
+rm $conf_file
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	#echo "ln -s ./SuiteSparse_config/SuiteSparse_config_Mac.mk $conf_file" 
+	ln -s SuiteSparse_config_Mac.mk $conf_file
+elif [[ "$OSTYPE" == "linux"* ]]; then
+	ln -s SuiteSparse_config_linux.mk $conf_file
+fi
+cd ..
 make
