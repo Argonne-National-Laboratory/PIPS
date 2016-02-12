5f1e2d6de857b17bb12993f0fb4d255c2c731964
Geoffrey Oxberry <oxberry1@llnl.gov>
2016-02-12 10:06:24 -0800

    Update OS X toolchain file
    Update OS X toolchain file to treat OpenMP and RPATH properly

--- OSX.cmake
+++ OSX.cmake
@@ -2,22 +2,25 @@ SET(CMAKE_C_COMPILER mpicc)
 SET(CMAKE_CXX_COMPILER mpicxx)
 SET(CMAKE_Fortran_COMPILER mpif90)
 
-# Set OpenMP flags for Clang/gcc; many people will build MPI w/ Clang
-set(OpenMP_CXX_FLAGS "-fopenmp")
-
-# Add Apple LAPACK/BLAS framework libraries
-set(MATH_LIBS "-framework Accelerate")
+# Taken from Elemental, courtesy of Jack Poulson
+set(CMAKE_MACOSX_RPATH TRUE)
 
 # use, i.e. don't skip the full RPATH for the build tree
-SET(CMAKE_SKIP_BUILD_RPATH FALSE)
+set(CMAKE_SKIP_BUILD_RPATH FALSE)
 
 # when building, don't use the install RPATH already
 # (but later on when installing)
-SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
 
-# the RPATH to be used when installing
-SET(CMAKE_INSTALL_RPATH "")
+# Add automatically determined parts of RPATH which point to directories
+# outside the build tree to the install RPATH
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 
-# don't add the automatically determined parts of the RPATH
-# which point to directories outside the build tree to the install RPATH
-SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH FALSE)
+# RPATH to be used when installing, but only if it's not a system directory
+list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES
+  "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
+if("${isSystemDir}" STREQUAL "-1")
+  SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+    endif()
+
+SET(CMAKE_MACOSX_RPATH ON)
