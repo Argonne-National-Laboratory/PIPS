Argonne-National-Laboratory/PIPS

commit 07930126ad1d12af084143aa78f97f5d2ca9aa81

adding debug message and format'

Input/amplGenStochInput.hpp
@@ -16,8 +16,6 @@ struct ASL_pfgh;
 struct SufDecl;
 struct SufDesc;
 
-
-
 #include "mpi.h"
 
 //  solve problem as general stochastic programming problem
@@ -40,64 +38,145 @@ struct SufDesc;
 //				     QW_n	QT_n
 //					  	  QA
 
-
-class amplGenStochInput : public stochasticInput {
+class amplGenStochInput: public stochasticInput {
 public:
-	amplGenStochInput(){}
-	amplGenStochInput(const std::string &datarootname, int overrideScenarioNumber = 0, 
-						MPI_Comm comm = MPI_COMM_WORLD);
-	~amplGenStochInput() { }
+	amplGenStochInput() {
+	}
+	amplGenStochInput(const std::string &datarootname,
+			int overrideScenarioNumber = 0, MPI_Comm comm = MPI_COMM_WORLD);
+	~amplGenStochInput() {
+	}
 
-	virtual int nScenarios() { return nScenarios_; }
-	virtual int nFirstStageVars() { return nFirstStageVars_; }
-	virtual int nFirstStageCons() { return nFirstStageCons_; }
-	virtual int nSecondStageVars(int scen) { return nSecondStageVars_[scen]; }
-	virtual int nSecondStageCons(int scen) { return nSecondStageCons_[scen]; }
+	virtual int nScenarios() {
+		return nScenarios_;
+	}
+	virtual int nFirstStageVars() {
+		return nFirstStageVars_;
+	}
+	virtual int nFirstStageCons() {
+		return nFirstStageCons_;
+	}
+	virtual int nSecondStageVars(int scen) {
+		return nSecondStageVars_[scen];
+	}
+	virtual int nSecondStageCons(int scen) {
+		return nSecondStageCons_[scen];
+	}
 
-	virtual std::vector<double> getFirstStageColLB(){ return firstStageData.collb; }
-	virtual std::vector<double> getFirstStageColUB(){ return firstStageData.colub; } 
-	virtual std::vector<double> getFirstStageObj(){ return firstStageData.objGrad; }
-	virtual std::vector<std::string> getFirstStageColNames(){return firstStageData.colnames;}
-	virtual std::vector<double> getFirstStageRowLB(){ return firstStageData.rowlb; }
-	virtual std::vector<double> getFirstStageRowUB(){ return firstStageData.rowub; }
-	virtual std::vector<std::string> getFirstStageRowNames(){ return firstStageData.rownames; }
-	
-	virtual bool isFirstStageColInteger(int col) { return false; }
+	virtual std::vector<double> getFirstStageColLB() {
+		return firstStageData.collb;
+	}
+	virtual std::vector<double> getFirstStageColUB() {
+		return firstStageData.colub;
+	}
+	virtual std::vector<double> getFirstStageObj() {
+		return firstStageData.objGrad;
+	}
+	virtual std::vector<std::string> getFirstStageColNames() {
+		return firstStageData.colnames;
+	}
+	virtual std::vector<double> getFirstStageRowLB() {
+		return firstStageData.rowlb;
+	}
+	virtual std::vector<double> getFirstStageRowUB() {
+		return firstStageData.rowub;
+	}
+	virtual std::vector<std::string> getFirstStageRowNames() {
+		return firstStageData.rownames;
+	}
 
-	virtual std::vector<double> getSecondStageColLB(int scen){loadLocalNLdata(scen);return localData[scen].collb;}
-	virtual std::vector<double> getSecondStageColUB(int scen){loadLocalNLdata(scen);return localData[scen].colub;}
-	virtual std::vector<double> getSecondStageObj(int scen){loadLocalNLdata(scen);return localData[scen].objGrad;}
-	virtual std::vector<std::string> getSecondStageColNames(int scen){loadLocalNLdata(scen);return localData[scen].colnames;}
-	virtual std::vector<double> getSecondStageRowLB(int scen){loadLocalNLdata(scen);return localData[scen].rowlb;}
-	virtual std::vector<double> getSecondStageRowUB(int scen){loadLocalNLdata(scen);return localData[scen].rowub;}
-	virtual std::vector<std::string> getSecondStageRowNames(int scen){loadLocalNLdata(scen);return localData[scen].rownames;}
-	virtual double scenarioProbability(int scen) { return 1.0/nScenarios_; }
-	virtual bool isSecondStageColInteger(int scen, int col) { return false; }
+	virtual bool isFirstStageColInteger(int col) {
+		return false;
+	}
 
+	virtual std::vector<double> getSecondStageColLB(int scen) {
+		loadLocalNLdata(scen);
+		return localData[scen].collb;
+	}
+	virtual std::vector<double> getSecondStageColUB(int scen) {
+		loadLocalNLdata(scen);
+		return localData[scen].colub;
+	}
+	virtual std::vector<double> getSecondStageObj(int scen) {
+		loadLocalNLdata(scen);
+		return localData[scen].objGrad;
+	}
+	virtual std::vector<std::string> getSecondStageColNames(int scen) {
+		loadLocalNLdata(scen);
+		return localData[scen].colnames;
+	}
+	virtual std::vector<double> getSecondStageRowLB(int scen) {
+		loadLocalNLdata(scen);
+		return localData[scen].rowlb;
+	}
+	virtual std::vector<double> getSecondStageRowUB(int scen) {
+		loadLocalNLdata(scen);
+		return localData[scen].rowub;
+	}
+	virtual std::vector<std::string> getSecondStageRowNames(int scen) {
+		loadLocalNLdata(scen);
+		return localData[scen].rownames;
+	}
+	virtual double scenarioProbability(int scen) {
+		return 1.0 / nScenarios_;
+	}
+	virtual bool isSecondStageColInteger(int scen, int col) {
+		return false;
+	}
 
+	virtual CoinPackedMatrix getFirstStageConstraints() {
+		PAR_DEBUG("getFirstStageConstraints");
+		Amat.dumpMatrix();
+		return Amat;
+	}
+	virtual CoinPackedMatrix getSecondStageConstraints(int scen) {
+		loadLocalNLdata(scen);
+		PAR_DEBUG("getSecondStageConstraints scen" << scen);
+		Wmat[scen].dumpMatrix();
+		return Wmat[scen];
+	}
+	virtual CoinPackedMatrix getLinkingConstraints(int scen) {
+		loadLocalNLdata(scen);
+		PAR_DEBUG("getLinkingConstraints scen" << scen);
+		Tmat[scen].dumpMatrix();
+		return Tmat[scen];
+	}
 
-	virtual CoinPackedMatrix getFirstStageConstraints() { return Amat; }
-	virtual CoinPackedMatrix getSecondStageConstraints(int scen) {loadLocalNLdata(scen); return Wmat[scen]; }
-	virtual CoinPackedMatrix getLinkingConstraints(int scen) {loadLocalNLdata(scen); return Tmat[scen]; }
-
-
-
-	virtual CoinPackedMatrix getFirstStageHessian(){return QAmat;}
+	virtual CoinPackedMatrix getFirstStageHessian() {
+		PAR_DEBUG("getFirstStageHessian");
+		QAmat.dumpMatrix();
+		return QAmat;
+	}
 	// Q_i
-	virtual CoinPackedMatrix getSecondStageHessian(int scen){loadLocalNLdata(scen);return QWmat[scen];}
+	virtual CoinPackedMatrix getSecondStageHessian(int scen) {
+		loadLocalNLdata(scen);
+		PAR_DEBUG("getSecondStageHessian scen" << scen);
+		QWmat[scen].dumpMatrix();
+		return QWmat[scen];
+	}
 	// column-oriented, \hat Q_i
 	// Note: this has the second-stage variables on the rows and first-stage on the columns
-	virtual CoinPackedMatrix getSecondStageCrossHessian(int scen){loadLocalNLdata(scen);return QTmat[scen];}
+	virtual CoinPackedMatrix getSecondStageCrossHessian(int scen) {
+		loadLocalNLdata(scen);
+		PAR_DEBUG("getSecondStageCrossHessian scen" << scen);
+		QTmat[scen].dumpMatrix();
+		return QTmat[scen];
+	}
 
-
-	virtual bool scenarioDimensionsEqual() { return dimsEqual; }
-	virtual bool onlyBoundsVary() { return false; } // no easy way to check
-	virtual bool allProbabilitiesEqual() { return true; }
-	virtual bool continuousRecourse() { return true; }
+	virtual bool scenarioDimensionsEqual() {
+		return dimsEqual;
+	}
+	virtual bool onlyBoundsVary() {
+		return false;
+	} // no easy way to check
+	virtual bool allProbabilitiesEqual() {
+		return true;
+	}
+	virtual bool continuousRecourse() {
+		return true;
+	}
 
 	virtual void doNetworkPart(const int scen, AmplSuffix* amplSuffix);
-
-
 
 protected:
 	bool dimsEqual;
@@ -109,52 +188,45 @@ protected:
 	std::vector<int> nSecondStageVars_, nSecondStageCons_;
 	std::vector<int*> LocGloVarIdx;
 
-	std::vector<std::map<int,int> > LocLocVarMap;
-	std::vector<std::map<int,int> > LocGloVarMap;
-	
-	std::vector<std::map<int,int> > LocWmatJacGoffMap;	
-	std::vector<std::map<int,int> > LocTmatJacGoffMap;	
+	std::vector<std::map<int, int> > LocLocVarMap;
+	std::vector<std::map<int, int> > LocGloVarMap;
 
+	std::vector<std::map<int, int> > LocWmatJacGoffMap;
+	std::vector<std::map<int, int> > LocTmatJacGoffMap;
 
-	std::vector<std::map<int,int> > LocQAmatHesGoffMap;
-	std::vector<std::map<int,int> > LocQWmatHesGoffMap;
-	std::vector<std::map<int,int> > LocQTmatHesGoffMap;	
-
-
+	std::vector<std::map<int, int> > LocQAmatHesGoffMap;
+	std::vector<std::map<int, int> > LocQWmatHesGoffMap;
+	std::vector<std::map<int, int> > LocQTmatHesGoffMap;
 
 	std::vector<int> decisionVarDim;
 	std::vector<int*> schurVarConIDinNL;
 
-
 	std::vector<int*> NR_partIDX_var;
 	std::vector<int*> NR_partIDX_con;
 
+	int mype_;
 
-    int mype_;
-	
 	std::vector<AmplData_NL> localData;
 	AmplData_NL firstStageData;
 
 	std::vector<ASL_pfgh*> asl_i;
 	ASL_pfgh* asl_0;
 
-	CoinPackedMatrix Amat, QAmat; 
+	CoinPackedMatrix Amat, QAmat;
 	std::vector<CoinPackedMatrix> Tmat, QTmat, Wmat, QWmat;
 
 	virtual void loadLocalNLdata(int scen);
 	virtual void splitMatrices(const int scen);
-	
 
-  	// no copying
+	// no copying
 	amplGenStochInput(const amplGenStochInput&);
 	amplGenStochInput& operator=(const amplGenStochInput&);
-
 
 ////////////////////////////////////////////////////////////////////////////////////
 
 	virtual void getRowMap(const int scen);
 
-	int nnzEqJac1st,nnzIneqJac1st;
+	int nnzEqJac1st, nnzIneqJac1st;
 	int* amplRowMap1st;
 
 	std::vector<int> nnzEqJac2nd, nnzIneqJac2nd;
@@ -172,22 +244,20 @@ protected:
 	int nnzALink1st, nnzCLink1st, nnzALoc1st, nnzCLoc1st;
 	std::vector<int> nnzALink2nd, nnzCLink2nd, nnzALoc2nd, nnzCLoc2nd;
 
-	std::map<int,int>  JacALinkGoff1st, JacCLinkGoff1st;
-	std::map<int,int>  JacALocGoff1st, JacCLocGoff1st;
+	std::map<int, int> JacALinkGoff1st, JacCLinkGoff1st;
+	std::map<int, int> JacALocGoff1st, JacCLocGoff1st;
 
-	std::vector<std::map<int,int> > JacALinkGoff2nd, JacCLinkGoff2nd;
-	std::vector<std::map<int,int> > JacALocGoff2nd, JacCLocGoff2nd;
-
+	std::vector<std::map<int, int> > JacALinkGoff2nd, JacCLinkGoff2nd;
+	std::vector<std::map<int, int> > JacALocGoff2nd, JacCLocGoff2nd;
 
 	int nnzQ1st;
 	std::vector<int> nnzQ2nd, nnzQCross2nd;
 
 ////////////////////////////////////////////////////////////////////////////////////	
-	
+
 	std::vector<int> starts1stSt;
 	friend class sNlpInfoFromNL;
 };
 
 #endif
-
 

commit a33670716d90198c6e2d2dc1a1576c3106080b2e

Input: Add missing header for PAR_DEBUG macro
Fixes bugs introduced by commit
07930126ad1d12af084143aa78f97f5d2ca9aa81. Before this commit, the
following build system errors occurred:

In file included from PIPS/Input/amplGenStochInput.cpp:5:

PIPS/Input/amplGenStochInput.hpp:128:3: error: use of undeclared
identifier 'PAR_DEBUG' PAR_DEBUG("getFirstStageConstraints");

PIPS/Input/amplGenStochInput.hpp:134:46: error:
invalid operands to binary expression ('const char *' and 'int')
PAR_DEBUG("getSecondStageConstraints scen" << scen);

PIPS/Input/amplGenStochInput.hpp:140:42: error: invalid
operands to binary expression ('const char *' and 'int')
PAR_DEBUG("getLinkingConstraints scen" << scen);

PIPS/Input/amplGenStochInput.hpp:146:3: error: use of undeclared
identifier 'PAR_DEBUG' PAR_DEBUG("getFirstStageHessian");

PIPS/Input/amplGenStochInput.hpp:153:42: error: invalid
operands to binary expression ('const char *' and 'int')
PAR_DEBUG("getSecondStageHessian scen" << scen);

PIPS/Input/amplGenStochInput.hpp:161:47: error: invalid
operands to binary expression ('const char *' and 'int')
PAR_DEBUG("getSecondStageCrossHessian scen" << scen);

 6 errors generated.
make[2]: ***
[Input/CMakeFiles/genNLparallel.dir/amplGenStochInput.cpp.o] Error 1
make[1]: *** [Input/CMakeFiles/genNLparallel.dir/all] Error 2 make: ***
[all] Error 2

Input/CMakeLists.txt
@@ -4,7 +4,7 @@ add_library(nlpstochInput rawInput.cpp combinedInput.cpp stochasticInput.cpp)
 
 add_library(genNLparallel amplGenStochInput.cpp amplGenStochInput_AddSlack.cpp stochasticInput.cpp AmplData_NL.cpp ${AMPL_LIBRARY})
 #target_include_directories(genNLparallel PUBLIC ${AMPL_INCLUDE_DIR})
-include_directories(${AMPL_INCLUDE_DIR})
+include_directories(${AMPL_INCLUDE_DIR} ../PIPS-NLP)
 
 add_library(genStructparallel StructJuMPInput.cpp stochasticInput.cpp)
 

Input/amplGenStochInput.hpp
@@ -11,6 +11,7 @@
 
 #include "stochasticInput.hpp"
 #include "AmplData_NL.hpp"
+#include "par_macro.h"
 
 struct ASL_pfgh;
 struct SufDecl;