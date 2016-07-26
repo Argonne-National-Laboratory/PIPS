bd2c448243177a0020d87cc4fbfe0ee9a9098e94
Cosmin G. Petra <petra1@llnl.gov>
2016-07-26 11:33:15 -0700

    robustified the boost-related cmake options

--- Toolchain.cmake
+++ Toolchain.cmake
@@ -1,1 +1,3 @@
-set(BOOST_ROOT "/sandbox/petra/work/installs/boost/")
+#set(Boost_USE_STATIC_LIBS ON) #use this option if have BOOST static libraries (unusual)
+set(BOOST_ROOT "/home/petra1/work/installs/boost_1_57_0/build")
+
