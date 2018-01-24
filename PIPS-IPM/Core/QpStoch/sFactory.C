f9d239ed13d1a0781f05c25dcaaffa67125421ef
Svenja Uslu <uslu@zib.de>
2018-01-24 16:36:30 +0100

    recreate ifdef timing in sFactory

--- PIPS-IPM/Core/QpStoch/sFactory.C
+++ PIPS-IPM/Core/QpStoch/sFactory.C
@@ -170,6 +170,12 @@ void dumpaug(int nx, SparseGenMatrix &A, SparseGenMatrix &C) {
 
 Data * sFactory::makeData()
 {
+#ifdef TIMING
+  double t2=MPI_Wtime();
+  int mype; MPI_Comm_rank(tree->commWrkrs,&mype);
+  bool p = (mype == 0);
+#endif
+
   // use the tree to get the data from user and to create OOQP objects
 
   //TIM;
