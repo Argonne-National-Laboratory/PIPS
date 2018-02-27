4887f6ed7ab2a5f2b809bfe5f4ce369a74a7b412
Svenja Uslu <uslu@zib.de>
2018-02-27 15:54:07 +0100

    add constness in find_blocking_pd

--- PIPS-IPM/Core/StochLinearAlgebra/StochVector.h
+++ PIPS-IPM/Core/StochLinearAlgebra/StochVector.h
@@ -77,14 +77,11 @@ public:
   virtual void findBlocking_pd(OoqpVector & wstep_vec,
   			      OoqpVector & u_vec,
   			      OoqpVector & ustep_vec,
-  			      double maxStepPri, double maxStepDual,
-  			      double *w_elt_p,
-  			      double *wstep_elt_p,
-  			      double *u_elt_p,
-  			      double *ustep_elt_p,
-  				  double *w_elt_d, double *wstep_elt_d, double *u_elt_d, double *ustep_elt_d,
-  				  double& stepPrimal, double& stepDual,
-				  bool& primalBlocking, bool& dualBlocking);
+  			      const double maxStepPri, const double maxStepDual,
+  			      double *w_elt_p, double *wstep_elt_p, double *u_elt_p, double *ustep_elt_p,
+  				   double *w_elt_d, double *wstep_elt_d, double *u_elt_d, double *ustep_elt_d,
+  				   double& stepPrimal, double& stepDual,
+				   bool& primalBlocking, bool& dualBlocking);
 
   virtual void componentMult( OoqpVector& v );
   virtual void componentDiv ( OoqpVector& v );
