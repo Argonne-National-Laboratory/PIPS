e291b0365f975efac9e83ce4f284d38d409902e2
Svenja Uslu <uslu@zib.de>
2018-05-14 15:56:40 +0200

    count singleton cols with bounds

--- PIPS-IPM/Core/QpPreprocess/StochPresolverSingletonColumns.h
+++ PIPS-IPM/Core/QpPreprocess/StochPresolverSingletonColumns.h
@@ -9,6 +9,8 @@
 #define PIPS_IPM_CORE_QPPREPROCESS_STOCHPRESOLVERSINGLETONCOLUMNS_H_
 
 #include "StochPresolverBase.h"
+#include <limits>
+
 
 class StochPresolverSingletonColumns : public StochPresolverBase
 {
@@ -21,8 +23,11 @@ public:
    virtual bool applyPresolving(int& nelims);
 
 private:
-   void initSingletonColumns(int& nSColEq, int& nSCIneq, int& nZeroCostSc);
-   void initSingletonColsBlock(int it, SimpleVector const * nnzColSimple, int& nSColEq, int& nSCIneq, int& nZeroCostSc);
+   void initSingletonColumns(int& nSColEq, int& nSCIneq, int& nZeroCostSc, int& nLowerBound, int& nUpperBound,
+         int& nBothBounds, int& nNoBounds, int& nSColEqLinkRow, int& nSColIneqLinkrow, int& nSC);
+   void initSingletonColsBlock(int it, SimpleVector const * nnzColSimple, int& nSColEq, int& nSCIneq, int& nZeroCostSc,
+         int& nLowerBound, int& nUpperBound, int& nBothBounds, int& nNoBounds, int& nSColEqLinkRow, int& nSColIneqLinkrow, int& nSC);
+   void synchronizeSumSeveral(int& val0, int& val1, int& val2, int& val3, int& val4, int& val5, int& val6, int&val7, int& val8, int& val9 );
 
 };
 
