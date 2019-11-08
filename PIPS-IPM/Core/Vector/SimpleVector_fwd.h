c236df046fd2f3d78fbf93c579e9a086d302b38d
Nils-Christian Kempke <bzfkempk@zib.de>
2019-11-08 21:47:43 +0100

    add missing files

---
+++ PIPS-IPM/Core/Vector/SimpleVector_fwd.h
@@ -0,0 +1,16 @@
+/*
+ * SimpleVector_fwd.h
+ *
+ *  Created on: 08.11.2019
+ *      Author: bzfkempk
+ */
+ 
+ #ifndef SIMPLEVECTOR_FWD_H
+ #define SIMPLEVECTOR_FWD_H
+ 
+ template<typename T>
+ class SimpleVectorBase;
+ 
+ using SimpleVector = SimpleVectorBase<double>;
+ 
+ #endif
---
+++ PIPS-IPM/Core/Vector/StochVector_fwd.h
@@ -0,0 +1,16 @@
+/*
+ * StochVector_fwd.h
+ *
+ *  Created on: 08.11.2019
+ *      Author: bzfkempk
+ */
+ 
+ #ifndef STOCHVECTOR_FWD_H
+ #define STOCHVECTOR_FWD_H
+ 
+ template<typename T>
+ class StochVectorBase;
+ 
+ using StochVector = StochVectorBase<double>;
+ 
+ #endif
