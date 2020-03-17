f73f9ca72c58640f8962650d0cde551eae2d4b49
Nils-Christian Kempke <bzfkempk@zib.de>
2020-03-17 13:46:26 +0100

    quickfix #90 so that example runs through

--- PIPS-IPM/Core/QpStoch/sData.C
+++ PIPS-IPM/Core/QpStoch/sData.C
@@ -2295,7 +2295,7 @@ void sData::initDistMarker(int blocksStart, int blocksEnd)
 
    assert(linkStartBlockIdA.size() > 0 && linkStartBlockIdC.size() > 0);
 
-   assert(blocksStart >= 0 && blocksStart < blocksEnd && blocksEnd <= static_cast<int>(linkStartBlockIdA.size()));
+   assert(blocksStart >= 0 && blocksStart < blocksEnd); // && blocksEnd <= static_cast<int>(linkStartBlockIdA.size()));
 
    const int nx0 = getLocalnx();
    const int my0 = getLocalmy();
