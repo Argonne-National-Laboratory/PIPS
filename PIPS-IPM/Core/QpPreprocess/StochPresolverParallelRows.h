9c396d2ad8dba4909036ae8b46b4feca64f3e4ec
Nils-Christian Kempke <bzfkempk@zib.de>
2020-03-23 14:48:18 +0100

    try fix a bug that complains about const non-const in rowlib on juwels

--- PIPS-IPM/Core/QpPreprocess/StochPresolverParallelRows.h
+++ PIPS-IPM/Core/QpPreprocess/StochPresolverParallelRows.h
@@ -37,14 +37,14 @@ namespace rowlib
 
     struct rowWithEntries
     {
-       int id;
-       int offset_nA;
-       int lengthA;
-       int* colIndicesA;
-       double* norm_entriesA;
-       int lengthB;
-       int* colIndicesB;
-       double* norm_entriesB;
+       const int id;
+       const int offset_nA;
+       const int lengthA;
+       const int* colIndicesA;
+       const double* norm_entriesA;
+       const int lengthB;
+       const int* colIndicesB;
+       const double* norm_entriesB;
 
         rowWithEntries(int id, int offset, int lenA, int* colA, double* entA, int lenB, int* colB, double* entB)
             : id(id), offset_nA(offset), lengthA(lenA), colIndicesA(colA), norm_entriesA(entA),
